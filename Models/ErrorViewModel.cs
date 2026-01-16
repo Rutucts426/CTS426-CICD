4cc6c7ebe167723619868b47ffee6b129686224e
= <=>
2026-01-16 08:18:23 +0000

    DemoWebapp426

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webapp426.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
